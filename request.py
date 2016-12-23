2503044a462d99a04e204f962c7b71469b5a8959
xavi <civit70@gmail.com>
2016-12-23 15:38:12 +0100

    Changes to be committed: 	modified:   request.py

--- request.py
+++ request.py
@@ -1,9 +1,31 @@
 # -*- coding: utf-8 -*-
 #pip install twython
 
+
+def getLastQueryFileName():
+	import os
+	onlyfiles = [f for f in os.listdir("scrapResults") if os.path.isfile(os.path.join("scrapResults", f))]
+	if len(onlyfiles)==0:
+		prevResults = False
+		dayLast=""
+		hourLast=""
+	else:
+		prevResults = True
+		[dayLast,hourLast] = onlyfiles[0].split(".")[0].split("_")
+		for file in onlyfiles:
+			[day,hour] = file.split(".")[0].split("_")
+			if(int(day)>int(dayLast)):
+				dayLast = day
+				hourLast = hour
+			elif(int(day) == int(dayLast)):
+				if(int(hour)>int(hourLast)):
+					dayLast = day
+					hourLast = hour
+	return [prevResults,"scrapResults/"+dayLast+"_"+hourLast+".json"]
+
 def getListOfSearch():
-	catalunya = ["Catalufo", "Catalunya", "Cataluña", "Catalanes", "Catalan", "Catalufos", "Polaco", "Polacos", "Indepe", "Indepes", "Independentista", "Independentistas", "Charnego", "Xarnego", "Txarnego", "Charnegos", "Xarnegos", "Txarnegos", "Catala", "Catalans", "Independentistes"]
-	puta = ["Puto", "Putos", "Puta", "Putas", "Mierda", "Hijoputa", "Hijos", "Hijo", "Joputa", "Joputas","Agarrados","Mierdas", "Tacaño", "muertos", "muerte", "morid", "mueran", "murais"]
+	catalunya = ["Catalufo"]#, "Catalunya", "Cataluña", "Catalanes", "Catalan", "Catalufos", "Polaco", "Polacos", "Indepe", "Indepes", "Independentista", "Independentistas", "Charnego", "Xarnego", "Txarnego", "Charnegos", "Xarnegos", "Txarnegos", "Catala", "Catalans", "Independentistes"]
+	puta = ["Puto", "Putos"]#, "Puta", "Putas", "Mierda", "Hijoputa", "Hijos", "Hijo", "Joputa", "Joputas","Agarrados","Mierdas", "Tacaño", "muertos", "muerte", "morid", "mueran", "murais"]
 
 	return [cat+" "+p for cat in catalunya for p in puta]
 
@@ -18,29 +40,33 @@ def connect():
 
 	return twitter
 
-def scrapNTimes(twitter,n,search):
-	import time
+def scrapNTimes(twitter,n,search,lastResults):
 	#Query---------------------------------------------------------------
-
 	for i in range(n):
-		try:
-			results = twitter.search(count=5000, q=search)
-			print "Total number of tweet found: ", len(results["statuses"])
-			
-			if(len(results["statuses"])==0):
-				break
-
-
-
-			if(len(results["statuses"])>=100):
-				print search, "hem trobat 100 tuits nous !!!!!!"
-			return results
-
-		except:
-			print "Error occured trying"
-			print "Sleeping..."
-			time.sleep(60*15)
+		#try:
+		results = twitter.search(count=5000, q=search)
+		print "Total number of tweet found: ", len(results["statuses"])
 		
+		if(len(results["statuses"])==0):
+			return [1,results]
+		
+		if "statuses" in lastResults:
+			elements2Delete=[]
+			for result in results["statuses"]:
+				for lastresult in lastResults["statuses"]:
+					if(result["id"]==lastresult["id"]):
+						elements2Delete.append(lastresult)
+			
+			for element in elements2Delete:
+				lastResults["statuses"].pop(lastResults["statuses"].index(element))
+			
+			if(len(lastResults["statuses"])>=100 ):
+				print search, "hem perdut tuits !!!!!!"
+		
+		return [1,results]
+
+		#except:
+		#	return [0,0]
 
 def main():
 	import time
@@ -53,16 +79,49 @@ def main():
 
 	#conectem
 	twitter = connect()
-
-	#nom del fitxer ANY MES DIA_HORA MINUT SEGON
-	fileName = "scrapResults/"+time.strftime("%Y%m%d_%H%M%S", time.gmtime())+".txt"
-	f = open(fileName, 'w')
 	
+	#previous search
+	[lastExist, fileNameLast]=getLastQueryFileName()
+	if lastExist:
+		print "Last query is: ", fileNameLast
+		fL = open(fileNameLast)
+		lastResult=json.load(fL)
+		fL.close()
+	else:
+		lastResult={}
+		for search in getListOfSearch():
+			lastResult[search]={}
+
+	
+	dicResults = {}
 	for search in getListOfSearch():
 		print "Searching for: ", search
 		#10 nombre dintents que fer per reconectar en cada busqueda
-		results = scrapNTimes(twitter,10,search)
-		json.dump(results, f)	
+		[queryDone,results] = scrapNTimes(twitter,10,search,lastResult[search])
+		if queryDone:
+			dicResults[search]=results
+		else:
+			print "ERROR DE CONEXIO"
+			time.sleep(60*15)
+			twitter = connect()
+			[queryDone,results] = scrapNTimes(twitter,10,search,lastResult[search])
+			if queryDone:
+				print "ERROR DE CONEXIO ARRETGLAT"
+				dicResults[search]=results
+			else:
+				print "ERROR DE CONEXIO PESISTEIX"
+				dicResults[search]={}
+	
+	#nom del fitxer ANY MES DIA_HORA MINUT SEGON
+	fileName = "scrapResults/"+time.strftime("%Y%m%d_%H%M%S", time.gmtime())+".json"
+	f = open(fileName,"w")
+	json.dump(dicResults, f)	
+
+	if lastExist:
+		fL = open(fileNameLast,"w")
+		json.dump(lastResult, fL)	
+
+
 		
 if __name__ == "__main__":
     main()
